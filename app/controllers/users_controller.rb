f1ed9d0c61edacd150ec8bacc03f4c7446c9d861
Bernd Braegelmann <bernd@braegelmann.de>
2011-11-23 00:12:53 +0100

    user validation and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.order(:name)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -41,14 +41,16 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(params[:user])
-
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
+        format.html { redirect_to users_url,
+          notice: "User #{@user.name} was successfully created." }
+        format.json { render json: @user,
+          status: :created, location: @user }
       else
         format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @user.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -60,11 +62,13 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to users_url,
+          notice: "User #{@user.name} was successfully updated." }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @user.errors,
+          status: :unprocessable_entity }
       end
     end
   end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,7 +1,20 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 require 'test_helper'
-
 class UsersControllerTest < ActionController::TestCase
   setup do
+    @input_attributes = {
+      name:                  "sam",
+      password:              "private",
+      password_confirmation: "private"
+    }
+
     @user = users(:one)
   end
 
@@ -16,12 +29,13 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  #...
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: @user.attributes
+      post :create, user: @input_attributes
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to users_path
   end
 
   test "should show user" do
@@ -34,9 +48,11 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+
+  #...
   test "should update user" do
-    put :update, id: @user.to_param, user: @user.attributes
-    assert_redirected_to user_path(assigns(:user))
+    put :update, id: @user.to_param, user: @input_attributes
+    assert_redirected_to users_path
   end
 
   test "should destroy user" do
