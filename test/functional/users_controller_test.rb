1ab6eaf7fde54a9bdfb90daea075491920cd1c0d
Bernd Braegelmann <bernd@braegelmann.de>
2011-11-23 03:11:51 +0100

    restricting access to user model for valid users

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -19,43 +19,43 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
-    get :index
+    get :index, {}, {'user_id' => users(:one).id}
     assert_response :success
     assert_not_nil assigns(:users)
   end
 
   test "should get new" do
-    get :new
+    get :new, {}, {'user_id' => users(:one).id}
     assert_response :success
   end
 
   #...
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: @input_attributes
+      post :create, {user: @input_attributes}, {'user_id' => users(:one).id}
     end
 
     assert_redirected_to users_path
   end
 
   test "should show user" do
-    get :show, id: @user.to_param
+    get :show, {id: @user.to_param}, {'user_id' => users(:one).id}
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @user.to_param
+    get :edit, {id: @user.to_param}, {'user_id' => users(:one).id}
     assert_response :success
   end
 
   test "should update user" do
-    put :update, id: @user.to_param, user: @input_attributes
+    put :update, {id: @user.to_param, user: @input_attributes}, {'user_id' => users(:one).id}
     assert_redirected_to users_path
   end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
-      delete :destroy, id: @user.to_param
+      delete :destroy, {id: @user.to_param}, {'user_id' => users(:one).id}
     end
 
     assert_redirected_to users_path
