26b167d427a66a14c8bf7a4b97109cc0e21b5afc
Bernd Braegelmann <bernd@braegelmann.de>
2011-11-23 01:14:07 +0100

    session controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ PerformanceCurve::Application.routes.draw do
   resources :users
 
   resources :fatigues
-
+  
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
